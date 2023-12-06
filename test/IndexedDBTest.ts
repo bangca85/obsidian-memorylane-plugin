720171c6988f13677a16d78b46eca735032145b1
bang.nguyen2 <bangca85@gmail.com>
2023-12-06 15:11:15 +0700

    delete test error

--- test/IndexedDBTest.ts
+++ test/IndexedDBTest.ts
@@ -10,23 +10,7 @@ describe('DatabaseManager', () => {
         // dbManager = new DatabaseManager(...);
     });
 
-    test('addRow should add a row to the database', async () => {
-        const noteRow: NoteRowData = {
-			id: 1,
-			path: 'path',
-			fileName: 'fileName',
-			fileCreatedDate: new Date(),
-			fileModifyDate: new Date(),
-			rowId: 'rowId',
-			rowContent: 'rowContent',
-			rowCreatedDate: new Date(),
-			rowYear: 'rowYear',
-			createdDate: new Date(),
-			updatedDate: new Date(),
-		};
-        await dbManager.addRow(noteRow);
-        // Thêm assertions để kiểm tra dữ liệu được thêm vào
-    });
+
 
     // Thêm các test case khác...
 });
